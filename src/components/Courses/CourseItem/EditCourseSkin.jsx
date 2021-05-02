6efc3096f1c9fb529b440f017b72a2df3fbcb04e
nashuarone <nashuarash@gmail.com>
2021-05-02 22:25:56 +0500

    some progress to courses module

--- src/components/Courses/CourseItem/EditCourseSkin.jsx
+++ src/components/Courses/CourseItem/EditCourseSkin.jsx
@@ -1,23 +1,22 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { compose } from "redux";
 import { useSelector, useDispatch } from "react-redux";
-import { withRouter, NavLink } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import ReactPlayer from "react-player";
-import { CKEditor } from "@ckeditor/ckeditor5-react";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import s from "../Courses.module.css";
 import { addUserCourse, getAllChapters } from "../../../redux/coursesReducer";
-import EditCourseItem from "./EditCourseItem";
-//import { getAllCoursesAPI } from "../../api/api";
 
 const CourseItem = (props) => {
-  //const [courseItemTitle, setCourseItemTitle] = useState("");
   const isTeacher = useSelector((s) => s.profilePage.isTeacher);
   const currentLearnerId = useSelector((s) => s.profilePage.profileData.id);
-  const courseChaptersData = useSelector((s) => s.coursesPage.courseChaptersData);
+  const courseChaptersData = useSelector(
+    (s) => s.coursesPage.courseChaptersData
+  );
 
   let courseId = props.match.params.courseId;
-  const coursesData = useSelector((s) => s.coursesPage.coursesData).filter(it => it.id === +courseId);
+  const coursesData = useSelector((s) => s.coursesPage.coursesData).filter(
+    (it) => it.id === +courseId
+  );
 
   const dispatch = useDispatch();
   useEffect(() => {
@@ -26,25 +25,6 @@ const CourseItem = (props) => {
   return (
     <div className={s.coursesPage}>
       <div className={s.editBlockTitle}>Текущий курс</div>
-      <NavLink to={"/courses"}>
-        <div title="выход из редактора" className={s.editBlockSubtitle}>
-          <i className="fas fa-arrow-alt-circle-left"></i> На страницу курсов
-        </div>
-      </NavLink>
-      <div className={s.editBlock}>
-        {isTeacher && (
-          <div className={s.editModeRed}>
-            <EditCourseItem courseId={courseId} />
-          </div>
-        )}
-      </div>
-      <div className={s.editBlock}>
-        {isTeacher && (
-          <div className={s.editModePurple}>
-            <EditCourseItem courseId={courseId} />
-          </div>
-        )}
-      </div>
       <div>
         {coursesData.map((c) => (
           <div className={s.courseItem} key={c.id}>
@@ -68,24 +48,6 @@ const CourseItem = (props) => {
                 <span className={s.tittleDecor}>{c.title}</span>
                 <div className={s.courseDescription}>{c.description}</div>
               </div>
-            </div>
-            <div>
-              {courseChaptersData.length > 0
-                ? courseChaptersData.map((chap) => (
-                    <div key={chap.id}>
-                      <div className={s.boldItemTitle}>{chap.title}</div>
-                      <div>
-                        {/* <div>{chap.content}</div> */}
-                        {/* <div className='editor' dangerouslySetInnerHTML={{__html: chap.content}}></div> */}
-                        <CKEditor
-                          editor={ClassicEditor}
-                          data={chap.content}
-                          disabled={true}
-                        />
-                      </div>
-                    </div>
-                  ))
-                : "Курс не содержит материалов"}
             </div>
             <div>
               <button
